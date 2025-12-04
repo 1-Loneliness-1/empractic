ec32eaed8c18e744c5b2032ffd98af621d29fd37
Kisurin Konstantin <costya.kisurin@yandex.ru>
2025-12-04 10:06:55 +0300

    fix(task2): Added coroutine scope transfer from the calling code and fixed the program startup time output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 package org.example
 
+import kotlinx.coroutines.cancel
 import org.example.model.*
 import org.example.utils.TimeConverter
 
 
 fun main() {
-    val appStartTime: Long by AppStartTimeDelegate()
+    val appStartTime: String by AppStartTimeDelegate(AppLevelScope)
     val listWithManyTypes: List<Any> = listOf(
         Student("13235", "Peter", 22),
         "Some string",
@@ -18,17 +19,20 @@ fun main() {
         14L
     )
 
+
+
     // Task 1 data класс может быть использован, но это не безопасно, потому что свойство field2 можно изменить
     // что приведет к тому, что мы не сможем больше получать значение по ключу
-    val key1 = Key(11, "key1")
-    val key2 = Key(22, "key2")
-    val someMap: HashMap<Key, String> = hashMapOf(key1 to "value1", key2 to "value2")
-    println(someMap.get(key1))
-    key1.field2 = "changedKey1"
-    println(someMap.get(key1))
+    // val key1 = Key(11, "key1")
+    // val key2 = Key(22, "key2")
+    // val someMap: HashMap<Key, String> = hashMapOf(key1 to "value1", key2 to "value2")
+    // println(someMap.get(key1))
+    // key1.field2 = "changedKey1"
+    // println(someMap.get(key1))
     // Task 2
-    // println("App was launched in: ${TimeConverter.timestampToHhmmss(appStartTime)}")
-    // Thread.sleep(10000)
+    println("App was launched in: $appStartTime")
+    Thread.sleep(10000)
+    AppLevelScope.cancel()
     // Task 3
     // println("Press Enter to view the first Int in the list.")
     // readlnOrNull()
--- src/main/kotlin/utils/TimeConverter.kt
+++ src/main/kotlin/utils/TimeConverter.kt
@@ -15,12 +15,4 @@ object TimeConverter {
             .format(timeFormatter)
     }
 
-    fun durationToHhmmssFormat(currentTime: Long): String {
-        val totalSeconds = currentTime / 1000
-        val hours = totalSeconds / 3600
-        val minutes = (totalSeconds % 3600) / 60
-        val seconds = totalSeconds % 60
-        return "%02d:%02d:%02d".format(hours, minutes, seconds)
-    }
-
 }
